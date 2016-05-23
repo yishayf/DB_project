2a97a59dce95268b5a3e812aa8d46f76eadf1514
sharon <sharonaftaly@gmail.com>
2016-05-23 23:39:08 +0300

    updated the flow so that athlete games also conatin the field_id

--- db_tests/olympic_games_test.py
+++ db_tests/olympic_games_test.py
@@ -13,11 +13,6 @@ con = mdb.connect('localhost', 'root', '', 'db_project_test',unix_socket = '/opt
 
 def query_and_insert_athlete_field_and_games():
     with con:
-        cur = con.cursor()
-        # cur.execute("TRUNCATE TABLE AthleteSportField")
-        # cur.execute("TRUNCATE TABLE AthleteGames")
-        # cur.execute("TRUNCATE TABLE SportField")
-        # con.commit()
         offset = 0
         while True:
             print offset
@@ -33,7 +28,6 @@ def query_and_insert_athlete_field_and_games():
 
 def get_athletes_games(offset):
     athlete_games_list = []
-
     query_offset_string = "PREFIX dbpedia0: <http://dbpedia.org/ontology/> " \
         "PREFIX dct: <http://purl.org/dc/terms/> " \
         "SELECT ?personlabel ?gamelabel WHERE { " \
@@ -71,26 +65,26 @@ def insert_athletes_games_and_field(athlete_game_tuple, con):
         year = match.group(2)
         season = match.group(3)
         cur = con.cursor()
-
         try:
             #insert into sport field
             cur.execute("INSERT IGNORE INTO SportField (field_name) VALUES (%s)", [field])
             con.commit()
 
-            # insert into athlete field - this will fail if athlete is not found
-            # field is necessarily found because
+            # insert into athlete column - this will fail if athlete is not found
+            # field is necessarily found because we already have it from previuos query
             cur.execute("INSERT IGNORE INTO AthleteSportFields (athlete_id, field_id) "
                         "SELECT a.athlete_id, f.field_id "
-                        "FROM athlete a, SportField f "
+                        "FROM Athlete a, SportField f "
                         "WHERE a.dbp_label = %s AND f.field_name = %s ",
                         (athlete_label, field))
             con.commit()
 
-            cur.execute("INSERT IGNORE INTO AthleteGames (athlete_id, game_id) "
-                        "SELECT a.athlete_id, g.game_id "
-                        "FROM athlete a, olympicgame g "
-                        "WHERE a.dbp_label = %s AND g.year = %s AND g.season = %s",
-                        (athlete_label, year, season))
+            # insert into game and field columns - this will fail if athlete is not found
+            cur.execute("INSERT IGNORE INTO AthleteGames (athlete_id, game_id, field_id) "
+                        "SELECT a.athlete_id, g.game_id, f.field_id "
+                        "FROM Athlete a, OlympicGame g, SportField f "
+                        "WHERE a.dbp_label = %s AND g.year = %s AND g.season = %s AND f.field_name = %s",
+                        (athlete_label, year, season, field))
             con.commit()
 
         except:
@@ -98,8 +92,13 @@ def insert_athletes_games_and_field(athlete_game_tuple, con):
             con.rollback()
 
 
+def query_and_insert_olympic_games():
+    # todo: make sure we update the cities
+    olympic_game_tuples = get_olympic_games()
+    insert_olympic_games(olympic_game_tuples)
 
-def get_olympic_years():
+
+def get_olympic_games():
     sparql.setQuery("""
     PREFIX dbpedia0: <http://dbpedia.org/ontology/>
 
@@ -124,12 +123,19 @@ def get_olympic_years():
     return olympic_games
 
 
-def query_and_insert_athletes():
+def insert_olympic_games(olympic_game_tuples):
     with con:
         cur = con.cursor()
+        for tup in olympic_game_tuples:
+            try:
+                cur.execute("INSERT INTO OlympicGame (year, season) VALUES (%s, %s)", (tup[0], tup[1]))
+                con.commit()
+            except:
+                con.rollback()
 
-        cur.execute("TRUNCATE TABLE athlete;")
-        con.commit()
+
+def query_and_insert_athletes():
+    with con:
         offset = 0;
         while True:
             print offset
@@ -138,7 +144,7 @@ def query_and_insert_athletes():
             athlete_list = get_athletes(offset)
             if athlete_list:
                 offset += len(athlete_list)
-                insert_athletes(athlete_list, cur)
+                insert_athletes(athlete_list, con)
             else:
                 break
 
@@ -172,16 +178,18 @@ def get_athletes(offset):
     return athlete_list
 
 
-def insert_athletes(athlete_tuples, cur):
+def insert_athletes(athlete_tuples, con):
     #TODO: make sure we dont have the same item twice while insert
     for tup in athlete_tuples:
+        cur = con.cursor()
+        label = tup[0].encode('latin-1', 'ignore')
+        name = tup[0].encode('latin-1', 'ignore').split('(')[0]
+        bd = tup[1].encode('latin-1', 'ignore')
+        bp = tup[2].encode('latin-1', 'ignore')
+        comment = tup[3].encode('latin-1', 'ignore')
         try:
-            label = tup[0].encode('latin-1', 'ignore')
-            name = tup[0].encode('latin-1', 'ignore').split('(')[0]
-            bd = tup[1].encode('latin-1', 'ignore')
-            bp = tup[2].encode('latin-1', 'ignore')
-            comment = tup[3].encode('latin-1', 'ignore')
-            cur.execute("INSERT INTO athlete (dbp_label, name, birth_date, birth_place, comment) VALUES (%s, %s, %s, %s, %s)",
+            cur.execute("INSERT INTO Athlete (dbp_label, name, birth_date, birth_place, comment) "
+                        "VALUES (%s, %s, %s, %s, %s)",
                         (label, name, bd, bp, comment))
             con.commit()
         except Exception as e:
@@ -189,128 +197,100 @@ def insert_athletes(athlete_tuples, cur):
             con.rollback()
 
 
-def insert_olympic_years(olympic_game_tuples):
-    #TODO: make sure we dont have the same item twice while insert
-    with con:
-        cur = con.cursor()
-        cur.execute("TRUNCATE TABLE olympicgame")
-        con.commit()
-        for tup in olympic_game_tuples:
-            try:
-                print tup
-                cur.execute("INSERT INTO olympicgame (year, season) VALUES (%s, %s)", (tup[0], tup[1]))
-                con.commit()
-            except:
-                con.rollback()
-
-
-def query_db():
-    con = mdb.connect('localhost', 'root', '', 'db_project_test')
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM OlympicGame")
-        for i in range(cur.rowcount):
-            row = cur.fetchone()
-            print row
-
-
-def run_query():
-
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    sparql.setQuery("""
-    PREFIX dbpedia0: <http://dbpedia.org/ontology/>
-
-    SELECT ?sw ?bd ?label WHERE {
-    ?sw a dbpedia0:Athlete.
-    ?sw dbpedia0:birthDate ?bd.
-    ?sw rdfs:label ?label.
-    FILTER(lang(?label) = "en")
-    FILTER(datatype(?bd) = xsd:date)
-    FILTER regex(?label, '^ab' , 'i')
-    }
-    GROUP BY ?sw
-    """)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-    print(len(results["results"]["bindings"]))
+# def query_db():
+#     con = mdb.connect('localhost', 'root', '', 'db_project_test')
+#     with con:
+#         cur = con.cursor()
+#         cur.execute("SELECT * FROM OlympicGame")
+#         for i in range(cur.rowcount):
+#             row = cur.fetchone()
+#             print row
+#
+#
+# def run_query():
+#     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+#     sparql.setQuery("""
+#     PREFIX dbpedia0: <http://dbpedia.org/ontology/>
+#
+#     SELECT ?sw ?bd ?label WHERE {
+#     ?sw a dbpedia0:Athlete.
+#     ?sw dbpedia0:birthDate ?bd.
+#     ?sw rdfs:label ?label.
+#     FILTER(lang(?label) = "en")
+#     FILTER(datatype(?bd) = xsd:date)
+#     FILTER regex(?label, '^ab' , 'i')
+#     }
+#     GROUP BY ?sw
+#     """)
+#     sparql.setReturnFormat(JSON)
+#     results = sparql.query().convert()
+#     print(len(results["results"]["bindings"]))
 
 
 def remove_foreign_keys():
     cur = con.cursor()
-    try:
-        cur.execute("ALTER TABLE AthleteGames DROP FOREIGN KEY ahtleteidconst;")
-        con.commit()
-    except:
-        traceback.print_exc(file=sys.stdout)
-        con.rollback()
-    try:
-        cur.execute("ALTER TABLE AthleteGames DROP FOREIGN KEY gameidconst;")
-        con.commit()
-    except:
-        traceback.print_exc(file=sys.stdout)
-        con.rollback()
-    try:
-        cur.execute("ALTER TABLE AthleteSportFields DROP FOREIGN KEY athleteidconst;")
-        con.commit()
-    except:
-        traceback.print_exc(file=sys.stdout)
-        con.rollback()
-    try:
-        cur.execute("ALTER TABLE AthleteSportFields DROP FOREIGN KEY fieldidconst;")
-        con.commit()
-    except:
-        traceback.print_exc(file=sys.stdout)
-        con.rollback()
+    drop_queries = [
+        "ALTER TABLE AthleteGames DROP FOREIGN KEY ahtleteidconst;",
+        "ALTER TABLE AthleteGames DROP FOREIGN KEY gameidconst;",
+        "ALTER TABLE AthleteGames DROP FOREIGN KEY fieldidconst1;",
+        "ALTER TABLE AthleteSportFields DROP FOREIGN KEY athleteidconst;",
+        "ALTER TABLE AthleteSportFields DROP FOREIGN KEY fieldidconst;"
+    ]
+    run_mysql_queries_lst(drop_queries)
 
 
 def add_foreign_keys():
-    cur = con.cursor()
-    # add foreign keys for AthleteGames
-    try:
-        cur.execute("ALTER TABLE `AthleteGames` ADD CONSTRAINT `ahtleteidconst` FOREIGN KEY(`athlete_id`) "
-                    "REFERENCES `db_project_test`. `athlete`(`athlete_id`) ON DELETE CASCADE ON UPDATE CASCADE; ")
-        con.commit()
-    except:
-        traceback.print_exc(file=sys.stdout)
-        con.rollback()
-    try:
-        cur.execute("ALTER TABLE `AthleteGames` ADD CONSTRAINT `gameidconst` FOREIGN KEY(`game_id`) "
-                    "REFERENCES `db_project_test`. `olympicgame`(`game_id`) ON DELETE CASCADE ON UPDATE CASCADE;")
-        con.commit()
-    except:
-        traceback.print_exc(file=sys.stdout)
-        con.rollback()
-        # add foreign keys for AthleteSportFields
-    try:
-        cur.execute("ALTER TABLE `AthleteSportFields` ADD CONSTRAINT `athleteidconst` FOREIGN KEY (`athlete_id`) "
-        "REFERENCES `db_project_test`.`athlete`(`athlete_id`) ON DELETE CASCADE ON UPDATE CASCADE;")
-        con.commit()
-    except:
-        traceback.print_exc(file=sys.stdout)
-        con.rollback()
-    try:
-        cur.execute("ALTER TABLE `AthleteSportFields` ADD CONSTRAINT `fieldidconst` FOREIGN KEY(`field_id`) "
-                   "REFERENCES `db_project_test`. `SportField`(`field_id`) ON DELETE CASCADE ON UPDATE CASCADE;")
-        con.commit()
-    except:
-        traceback.print_exc(file=sys.stdout)
-        con.rollback()
+    # add foreign keys for the needed tables
+    foreign_keys_add_queries = [
+        "ALTER TABLE `AthleteGames` ADD CONSTRAINT `ahtleteidconst` FOREIGN KEY(`athlete_id`) "
+        "REFERENCES `db_project_test`. `Athlete`(`athlete_id`) ON DELETE CASCADE ON UPDATE CASCADE; ",
+        "ALTER TABLE `AthleteGames` ADD CONSTRAINT `gameidconst` FOREIGN KEY(`game_id`) "
+        "REFERENCES `db_project_test`. `OlympicGame`(`game_id`) ON DELETE CASCADE ON UPDATE CASCADE;",
+        "ALTER TABLE `AthleteGames` ADD CONSTRAINT `fieldidconst1` FOREIGN KEY (`field_id`) "
+        "REFERENCES `db_project_test`.`SportField`(`field_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;",
+        "ALTER TABLE `AthleteSportFields` ADD CONSTRAINT `athleteidconst` FOREIGN KEY (`athlete_id`) "
+        "REFERENCES `db_project_test`.`Athlete`(`athlete_id`) ON DELETE CASCADE ON UPDATE CASCADE;",
+        "ALTER TABLE `AthleteSportFields` ADD CONSTRAINT `fieldidconst` FOREIGN KEY(`field_id`) "
+        "REFERENCES `db_project_test`. `SportField`(`field_id`) ON DELETE CASCADE ON UPDATE CASCADE;"
+    ]
+    run_mysql_queries_lst(foreign_keys_add_queries)
+
 
 def truncate_all_dbpedia_data_tables():
-    return
+    queries_lst = [
+        "TRUNCATE TABLE OlympicGame",
+        "TRUNCATE TABLE Athlete;",
+        "TRUNCATE TABLE SportField",
+        "TRUNCATE TABLE AthleteSportFields",
+        "TRUNCATE TABLE AthleteGames"
+    ]
+    run_mysql_queries_lst(queries_lst)
+
+
+def run_mysql_queries_lst(mysql_query_lst):
+    for query in mysql_query_lst:
+        run_mysql_query(query)
+
+
+def run_mysql_query(my_sql_query):
+    with con:
+        cur = con.cursor()
+        try:
+            cur.execute(my_sql_query)
+            con.commit()
+        except:
+            traceback.print_exc(file=sys.stdout)
+            con.rollback()
+
 
 # remove foreign keys from tables
 # remove_foreign_keys()
 
 # truncate tables
-truncate_all_dbpedia_data_tables()
+# truncate_all_dbpedia_data_tables()
 
-
-# get all olympic years
-# olympic_years = get_olympic_years()
-
-# insert olympic years
-# insert_olympic_years(olympic_years)
+# get all olympic years and insert to db
+# query_and_insert_olympic_games()
 
 # get and insert athletes
 # query_and_insert_athletes()
