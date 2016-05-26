e3592f4d9a237c466d2285bcb1cd4e05304eb0d1
nogalavi <noga.lavi1@gmail.com>
2016-05-26 12:29:03 +0300

    mock trivia is working

--- Front_End/js/quiz.js
+++
@@ -1,6 +0,0 @@
-
-function parseHttp(text, header) {
-    var obj = JSON.parse(text);
-    document.getElementById("header").innerHTML = obj; //.employees[1].firstName + " " + obj.employees[1].lastName;
-
-}
