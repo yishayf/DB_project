7eb9884a05b3ffd46af3a38b25c54af34ea25cd8
Yishay <yishayf@gmail.com>
2016-05-28 17:30:48 +0300

    after doing some of the dropdown logic

--- Front_End/js/ask_question.js
+++ Front_End/js/ask_question.js
@@ -1,26 +1,46 @@
-formats_http = "http://cs.tau.ac.il/~naftaly1/get_question_types.php";
+server_local = "http://localhost/DB_php"
+server_nova = "http://cs.tau.ac.il/~naftaly1"
+formats_http = server_local + "/get_question_types.php";
 firstArg_http = "http://cs.tau.ac.il/~naftaly1/get_question_types.php/?argbla=blabla";
+noga_array = "http://cs.tau.ac.il/~nogalavi1/mockYearsNew.php";
 
 var app = angular.module('askQuestion', []);
 
 app.controller('askController', function($scope, $http) {
 
-    $scope.DropDownDiabled = true;
+    //$scope.DropDownDiabled = true;
 
     $scope.createFormatsDropDown = function() {
-        $http.get(formats_http).then(function(d) {
+        $http.get(formats_http).then(function (d) {
             $scope.parseJason(d.data);
-            //var qTypeAnswer = "http://cs.tau.ac.il/~naftaly1/get_question_types.php/?q_type=" + $scope.questionTypeIndex;
-            //$http.get(qTypeAnswer).then(function(opt) {
-                //$scope.parseFirstArgumentJason(opt.data);
-        //});
-    });
-
-
+        });
+    }
+    
+    $scope.firstUpdated = function () {
+        console.log($scope.numsOfBlanks);
+        if ($scope.numsOfBlanks[$scope.questionTypeIndex] > 1){
+            var qTypeAnswer = "http://cs.tau.ac.il/~nogalavi1/mockYearsNew.php";
+            $http.get(qTypeAnswer).then(function (options) {
+                $scope.secondArgOptions = options.data;
+            });
+        }
+    }
+    
+    $scope.updateFirstArg = function () {
+        //var qTypeAnswer = "http://localhost/DB_php/get_question_types.php/?q_type=" + $scope.questionTypeIndex;
+        var qTypeAnswer = "http://cs.tau.ac.il/~nogalavi1/mockYearsNew.php";
+        $http.get(qTypeAnswer).then(function (options) {
+            $scope.firstArgOptions = options.data;
+        });
+    }
+        
+        
+        
     $scope.recreateArgs  = function () {
         $scope.questionTypeIndex = $scope.formats.indexOf($scope.selectedFormat);
         var numOfBlanks = $scope.numsOfBlanks[$scope.questionTypeIndex];
         $scope.showArgsDropDowns(numOfBlanks);
+        $scope.updateFirstArg();
     }
 
     $scope.showArgsDropDowns = function(numOfArgs) {
@@ -52,16 +72,10 @@ app.controller('askController', function($scope, $http) {
         $scope.args = args;
     }
         
+    $scope.parseFirstArgumentJason = function (data) {
         
-        
-        
-        
-        
-        
-        $scope.createFormatsDropDown();
-
-
     }
+    
+    $scope.createFormatsDropDown();
 });
-
 
