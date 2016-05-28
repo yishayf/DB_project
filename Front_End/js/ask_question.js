125cc4d0b5ee69641c0f0efb3facc842c9ad93b4
Yishay <yishayf@gmail.com>
2016-05-28 15:23:21 +0300

    commit before merging with php files

--- Front_End/js/ask_question.js
+++ Front_End/js/ask_question.js
@@ -1,4 +1,5 @@
 formats_http = "http://cs.tau.ac.il/~naftaly1/get_question_types.php";
+firstArg_http = "http://cs.tau.ac.il/~naftaly1/get_question_types.php/?argbla=blabla";
 
 var app = angular.module('askQuestion', []);
 
@@ -9,22 +10,25 @@ app.controller('askController', function($scope, $http) {
     $scope.createFormatsDropDown = function() {
         $http.get(formats_http).then(function(d) {
             $scope.parseJason(d.data);
-        });
-    }
+            //var qTypeAnswer = "http://cs.tau.ac.il/~naftaly1/get_question_types.php/?q_type=" + $scope.questionTypeIndex;
+            //$http.get(qTypeAnswer).then(function(opt) {
+                //$scope.parseFirstArgumentJason(opt.data);
+        //});
+    });
 
 
     $scope.recreateArgs  = function () {
-        var i = $scope.formats.indexOf($scope.selectedFormat);
-        var numOfBlanks = $scope.numsOfBlanks[i];
+        $scope.questionTypeIndex = $scope.formats.indexOf($scope.selectedFormat);
+        var numOfBlanks = $scope.numsOfBlanks[$scope.questionTypeIndex];
         $scope.showArgsDropDowns(numOfBlanks);
     }
 
     $scope.showArgsDropDowns = function(numOfArgs) {
         var i;
         $scope.dropDownArr = new Array(3);
-        for (i = 0; i < 3; i++){
-            if (i < numOfArgs){
-                $scope.dropDownArr[i] = true;
+        for (i = 0; i < 3; i++) {
+            if (i < numOfArgs) {
+                $scope.dropDownArr[i] = $scope.args[$scope.questionTypeIndex][i];
             }
             else {
                 $scope.dropDownArr[i] = false;
@@ -36,17 +40,28 @@ app.controller('askController', function($scope, $http) {
     $scope.parseJason = function(data) {
         var formats = [];
         var numsOfBlanks = [];
+        var args = [];
         var i;
         for (i = 0; i < data.length; i++) {
             formats.push(data[i].question_format);
             numsOfBlanks.push(data[i].num_args);
+            args.push(data[i].args);
         }
         $scope.formats = formats;
         $scope.numsOfBlanks = numsOfBlanks;
+        $scope.args = args;
     }
+        
+        
+        
+        
+        
+        
+        
+        $scope.createFormatsDropDown();
 
-    $scope.createFormatsDropDown();
 
-
+    }
 });
+
 
