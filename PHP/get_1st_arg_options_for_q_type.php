63d29a972e32a071ccb3cf1fe4a92d318415bf56
Sharon <sharonaftaly@gmail.com>
2016-05-28 15:29:36 +0300

    fixed an issue when connecting to db from php

--- PHP/get_1st_arg_options_for_q_type.php
+++ PHP/get_1st_arg_options_for_q_type.php
@@ -7,9 +7,10 @@ require_once 'mysql_general.php';
 function get_sql_query_for_args_by_q_type($q_type){
     switch ($q_type){
         case 1:
-            return "SELECT DISTINCT a.year 
-                FROM OlympicGame
-                WHERE city != ''; "; //TODO: change to != null ?
+            return "SELECT DISTINCT og.year FROM 
+                OlympicGame og, (SELECT year, season from OlympicGame WHERE
+                concat(year, season) not in (select concat(year, season) from Question_type1)) as VALID 
+                WHERE og.year = valid.year AND og.City != '';";
         case 2:
             break;
         case 3;
@@ -22,22 +23,25 @@ function get_sql_query_for_args_by_q_type($q_type){
 
 
 function get_1st_arg_options_by_q_type($q_type){
+    $sql_query = get_sql_query_for_args_by_q_type($q_type);
+    $result = run_sql_select_query($sql_query);
+    $res_array = array();
     switch ($q_type){
         case 1:
-            break;
-        case 2:
-            break;
-        case 3;
-            break;
-        case 4;
-            break;
-        case 5;
+            while ($row = $result->fetch_assoc()) {
+                array_push($res_array, $row['year']);
+            }
     }
+    return $res_array;
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "GET"){
-    $q_type = $_GET["q_type"];
-    $options = get_1st_arg_options_by_q_type($q_type);
-    echo json_encode($options);
-}
+//if ($_SERVER["REQUEST_METHOD"] == "GET"){
+//    $q_type = $_GET["q_type"];
+//    $options_arr = get_1st_arg_options_by_q_type($q_type);
+//    echo json_encode($options_arr);
+//}
+
+$options_arr = get_1st_arg_options_by_q_type(1);
+echo json_encode($options_arr);
+
 ?>
--- PHP/mysql_general.php
+++ PHP/mysql_general.php
@@ -1,5 +1,7 @@
 <?php
-require_once 'mysql_general.php';
+
+//$db = new mysqli("localhost", 'root', '', 'db_project_test');
+$db = new mysqli('mysqlsrv.cs.tau.ac.il', 'DbMysql08', 'DbMysql08', 'DbMysql08');  # for nova
 
 function run_sql_select_query($sql_query){
     global $db;
