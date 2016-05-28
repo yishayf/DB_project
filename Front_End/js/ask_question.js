3889bf24e06faf09171f66e89bc47ff78f2e903c
nogalavi <noga.lavi1@gmail.com>
2016-05-28 23:46:56 +0300

    Added an online changing question Started working on submit file

--- Front_End/js/ask_question.js
+++ Front_End/js/ask_question.js
@@ -1,14 +1,14 @@
 server_local = "http://localhost/DB_php"
 server_nova = "http://cs.tau.ac.il/~naftaly1"
-formats_http = server_local + "/get_question_types.php";
-firstArg_http = "http://cs.tau.ac.il/~naftaly1/get_question_types.php/?argbla=blabla";
-noga_array = "http://cs.tau.ac.il/~nogalavi1/mockYearsNew.php";
+server_sharon = "http://192.168.14.37/phpTest"
+formats_http = server_sharon + "/get_question_types.php";
+firstArg_http = server_sharon + "/get_1st_arg_options_for_q_type.php/?q_type=";
+secondArg_http = server_sharon + "/get_2nd_arg_options_for_q_type.php/?q_type=";
 
 var app = angular.module('askQuestion', []);
 
 app.controller('askController', function($scope, $http) {
 
-    //$scope.DropDownDiabled = true;
 
     $scope.createFormatsDropDown = function() {
         $http.get(formats_http).then(function (d) {
@@ -17,18 +17,37 @@ app.controller('askController', function($scope, $http) {
     }
     
     $scope.firstUpdated = function () {
-        console.log($scope.numsOfBlanks);
-        if ($scope.numsOfBlanks[$scope.questionTypeIndex] > 1){
-            var qTypeAnswer = "http://cs.tau.ac.il/~nogalavi1/mockYearsNew.php";
+        $scope.selectedSecondArg = null;
+        var questionType = $scope.questionTypeIndex;
+        if ($scope.numsOfBlanks[questionType] > 1){
+            var qTypeAnswer = secondArg_http + questionType +1 + "&arg1=" + $scope.selectedFirstArg;
             $http.get(qTypeAnswer).then(function (options) {
                 $scope.secondArgOptions = options.data;
             });
         }
+        $scope.updateAllArgsSelected();
     }
-    
+
+    $scope.secondUpdated = function() {
+        $scope.selectedThirdArg = null;
+        $scope.updateAllArgsSelected();
+    }
+
+
+    $scope.updateAllArgsSelected = function() {
+        var numArgs = $scope.numsOfBlanks[$scope.questionTypeIndex];
+        var one = (numArgs == 1 && ($scope.selectedFirstArg!=null));
+        var two = (numArgs == 2 && ($scope.selectedFirstArg!=null) && ($scope.selectedSecondArg!=null));
+        var three = (numArgs == 3 && ($scope.selectedFirstArg!=null) && ($scope.selectedSecondArg!=null) && ($scope.selectedThirdArg!=null));
+        console.log(one || two || three);
+        $scope.allArgsSelected =  one || two || three;
+    }
+
+
     $scope.updateFirstArg = function () {
         //var qTypeAnswer = "http://localhost/DB_php/get_question_types.php/?q_type=" + $scope.questionTypeIndex;
-        var qTypeAnswer = "http://cs.tau.ac.il/~nogalavi1/mockYearsNew.php";
+        var questionType = $scope.questionTypeIndex;
+        var qTypeAnswer = firstArg_http + (parseInt(questionType) + 1);
         $http.get(qTypeAnswer).then(function (options) {
             $scope.firstArgOptions = options.data;
         });
@@ -40,11 +59,14 @@ app.controller('askController', function($scope, $http) {
         $scope.selectedFirstArg = null;
         $scope.selectedSecondArg = null;
         $scope.selectedThirdArg = null;
+        $scope.allArgsSelected = false;
         $scope.questionTypeIndex = $scope.formats.indexOf($scope.selectedFormat);
         var numOfBlanks = $scope.numsOfBlanks[$scope.questionTypeIndex];
         $scope.showArgsDropDowns(numOfBlanks);
         $scope.updateFirstArg();
+        $scope.selectedFormatWithNumbers = $scope.wordsToNumbers($scope.selectedFormat);
     }
+
 
     $scope.showArgsDropDowns = function(numOfArgs) {
         var i;
@@ -58,6 +80,30 @@ app.controller('askController', function($scope, $http) {
             }
         }
     }
+
+    $scope.submit = function() {
+
+    }
+
+    $scope.wordsToNumbers = function() {
+        var s = $scope.selectedFormat;
+        var result = "";
+        var i;
+        var numOfmatches = 1;
+        for (i=0; i<s.length; i++ ) {
+            if (s.charAt(i) == '[') {
+                result += ('#'+numOfmatches);
+                numOfmatches += 1;
+                while (s.charAt(i) != ']') {
+                    i ++
+                }
+                i++;
+            }
+            result += s.charAt(i);
+        }
+        return result;
+    }
+
 
 
     $scope.parseJason = function(data) {
@@ -74,11 +120,8 @@ app.controller('askController', function($scope, $http) {
         $scope.numsOfBlanks = numsOfBlanks;
         $scope.args = args;
     }
-        
-    $scope.parseFirstArgumentJason = function (data) {
-        
-    }
     
+
     $scope.createFormatsDropDown();
 });
 
