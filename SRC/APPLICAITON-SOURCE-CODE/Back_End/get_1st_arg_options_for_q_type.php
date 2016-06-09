f7a236a25b670e5ad891cd8fce89c8f1d68e0e2c
Sharon <sharonaftaly@gmail.com>
2016-06-09 13:46:06 +0300

    reusing mysql_general.php

--- SRC/APPLICAITON-SOURCE-CODE/Back_End/get_1st_arg_options_for_q_type.php
+++ SRC/APPLICAITON-SOURCE-CODE/Back_End/get_1st_arg_options_for_q_type.php
@@ -3,65 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-// open db connection
-//$db = new mysqli("localhost", 'root', '', 'db_project_test');
-$db = new mysqli('mysqlsrv.cs.tau.ac.il', 'DbMysql08', 'DbMysql08', 'DbMysql08');  # for nova
-//$db = new mysqli('localhost', 'DbMysql08', 'DbMysql08', 'DbMysql08', 3305);  # for nova local
-
-// Check connection
-if ($db->connect_error) {
-    http_response_code(500);
-    die("Connection failed: " . $db->connect_error);
-}
-$db->set_charset('utf8');
-
-function execute_sql_statement(&$stmt){
-    global $db;
-    if(!$stmt->execute()){
-        $stmt->close();
-        http_response_code(500);
-        die('There was an error running the query [' . $db->error . ']');
-    }
-    $stmt->store_result();
-}
-
-function execute_sql_insert_or_update_statement(&$stmt){
-    global $db;
-    if(!$stmt->execute()){
-        http_response_code(500);
-        $stmt->close();
-        die('There was an error running the query [' . $db->error . ']');
-    }
-    $stmt->store_result();
-    return TRUE;
-}
-
-
-function prepare_stmt($stmt_text){
-    global $db;
-    if (!$stmt = $db->prepare($stmt_text)){
-        http_response_code(500);
-        die("Error:preparing query failed: (" . $stmt->errno . ") " . $db->error);
-    }
-    return $stmt;
-}
-
-/*
- * Utility function to automatically bind columns from selects in prepared statements to an array
- * CREDIT: https://gunjanpatidar.wordpress.com/2010/10/03/bind_result-to-array-with-mysqli-prepared-statements/
- */
-function bind_result_array($stmt){
-    $meta = $stmt->result_metadata();
-    $result = array();
-    while ($field = $meta->fetch_field())
-    {
-        $result[$field->name] = NULL;
-        $params[] = &$result[$field->name];
-    }
-    call_user_func_array(array($stmt, 'bind_result'), $params);
-    return $result;
-}
-
+require_once 'mysql_general.php';
 /////////////////////////////////////////////////////////////////////////////////////
 
 $options_limit = 100;
